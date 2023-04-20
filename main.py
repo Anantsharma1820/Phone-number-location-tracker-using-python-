fdbf41912a7cb8b7d348e0c434349616ce34d361
Anant Sharma <98807424+Anantannu@users.noreply.github.com>
2023-04-20 16:37:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import phonenumbers
+from myphone import number
+from phonenumbers import geocoder
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, "en")
+print(location)
+
+from phonenumbers import carrier
+service_pro = phonenumbers.parse(number)
+print(carrier.name_for_number(service_pro, "en"))
+
+
+
